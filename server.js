f1300dfcff3678505979a7c667269760a0b400c6
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-04-05 11:46:57 -0400

    POST method done

--- server.js
+++ server.js
@@ -10,38 +10,48 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/api/notes", function(req, res) {
-    fs.readFile("db/db.json", "utf8", function(err, data) {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
       if(err) throw err;
-      if(data.length > 2) {
+      else {
         let notesSave = JSON.parse(data);
         res.send(notesSave);
-      }
-      else {
-        console.log("No notes.")
       }
     })
   });
 
-app.post("/api/note", function(req, res) {
-
+app.post("/api/notes", (req, res) => {
+  const noteNew = req.body;
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    const noteArray = JSON.parse(data);
+    noteArray.push(noteNew);
+    noteArray.forEach((note, i) => {
+      note.id = i +1
+    });
+    const noteString = JSON.stringify(noteArray);
+    fs.writeFile("./db/db.json", noteString, "utf8", (err) => {
+      if (err) throw err;
+    });
+    res.json(noteArray);
+  });
+  
 });
 
 // HTML routes for the server
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-  });
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-  });
+// If no matching route is found default to home
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 
-app.listen(PORT, function() {
+app.listen(PORT, () => {
     console.log("App listening on PORT: " + PORT);
   });
 
